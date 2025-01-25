7be06c3e906b035c79d57b00b225799efe55a30b
Filip Jeremić <filipjeremic.it@gmail.com>
2025-01-25 11:11:16 +0100

    feat: added the Variables class
    it showcases valid and invalid variable names

---
+++ src/main/java/kettu/Variables.java
@@ -0,0 +1,28 @@
+package kettu;
+
+public class Variables {
+
+  @SuppressWarnings("unused")
+  public static void namesTest() {
+
+    // valid names
+    int myVar = 0;
+    int _myVar = 0;
+    int $myVar = 0;
+    int var1 = 0;
+    int MAX_SPEED = 0;
+    int $ = 0;
+    int π = 0;
+    int españa = 0;
+    int 变量 = 0;
+
+    // invalid names
+    // int 2var = 0; starts with a digit
+    // int @var = 0; invalid character
+    // int my Var = 0; spaces are not allowed
+    // int class = 0; class is a reserved keyword
+    // int _ = 0; '_' is a keyword from source level 9 onwards
+
+  }
+
+}

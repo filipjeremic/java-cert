e92a39e6b18186754b211d218afd0cedec22e22c
Filip Jeremić <filipjeremic.it@gmail.com>
2025-01-28 08:35:25 +0100

    feat: added one more Streams example
    it compares processing with and without streams

--- src/main/java/kettu/Streams.java
+++ src/main/java/kettu/Streams.java
@@ -1,5 +1,6 @@
 package kettu;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,6 +15,45 @@ public class Streams {
     List<Integer> numbers = Arrays.asList(7, 4, 1, 2, 3, 9, 5);
     numbers.stream().sorted().forEach(System.out::println);
 
+  }
+
+  public static void example2() {
+
+    System.out.println("----- example2 -----");
+
+    List<String> cars = Arrays.asList("mercedes", "volvo", "porsche", "fiat", "zastava", "lada", "peugeot");
+    System.out.println(cars);
+
+    List<String> processedCars1 = processWithoutStreams(cars);
+    System.out.println(processedCars1);
+
+    List<String> processedCars2 = processWithStreams(cars);
+    System.out.println(processedCars2);
+
+    System.out.println();
+
+  }
+
+  static List<String> processWithoutStreams(List<String> cars) {
+    List<String> processedCars = new ArrayList<>();
+    for (final String car : cars) {
+      String uppercaseCar = car.toUpperCase();
+      if (uppercaseCar.startsWith("P")) {
+        processedCars.add(uppercaseCar);
+      }
+    }
+    java.util.Collections.sort(processedCars);
+    return processedCars;
+  }
+
+  static List<String> processWithStreams(List<String> cars) {
+    return cars.stream()
+        .map(String::toUpperCase)
+        .filter(item -> item.startsWith("P"))
+        .sorted()
+        .toList();
+    // .toList() produces an unmodifiable list
+    // to get a modifiable list use .collect(Collectors.toList())
   }
 
 }

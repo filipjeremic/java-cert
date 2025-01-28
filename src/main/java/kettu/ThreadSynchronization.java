a0083256c259332ff75bd87de89dc5ce3fdbb76a
Filip Jeremić <filipjeremic.it@gmail.com>
2025-01-28 12:00:30 +0100

    feat: added a thread example with a race condition

---
+++ src/main/java/kettu/ThreadSynchronization.java
@@ -0,0 +1,45 @@
+package kettu;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadSynchronization {
+
+  private int value = 5;
+
+  private int getValue() {
+    return value;
+  }
+
+  private void addValue(final int x) {
+    value += x;
+  }
+
+  private void withoutSynchronization() {
+    final int currentValue = getValue();
+    System.out.println("Start value: " + currentValue);
+
+    if (currentValue < 10) {
+      addValue(10);
+      System.out.println("End value: " + getValue());
+    } else {
+      System.out.println("Value already too large");
+    }
+  }
+
+  public void testSequential() {
+    withoutSynchronization();
+    withoutSynchronization();
+  }
+
+  public void testWithoutSynchronization() {
+
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+    executorService.submit(() -> withoutSynchronization());
+    executorService.submit(() -> withoutSynchronization());
+
+    executorService.shutdown();
+  }
+
+}

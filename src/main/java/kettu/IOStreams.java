cac32fbeb92b35adf012e8264754263a062fe161
Filip Jeremić <filipjeremic.it@gmail.com>
2025-02-01 20:43:32 +0100

    feat: added an example which uses a BufferedReader with Streams

--- src/main/java/kettu/IOStreams.java
+++ src/main/java/kettu/IOStreams.java
@@ -3,6 +3,7 @@ package kettu;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 public class IOStreams {
 
@@ -11,6 +12,17 @@ public class IOStreams {
     try (BufferedReader reader = new BufferedReader(new FileReader("doesn't exist"))) {
       String firstLine = reader.readLine();
       System.out.println(firstLine);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public static void bufferedReaderAndStreamsExample() {
+
+    try (BufferedReader reader = new BufferedReader(new FileReader("doesn't exist"))) {
+      Stream<String> lines = reader.lines();
+      lines.forEach(System.out::println);
     } catch (IOException e) {
       e.printStackTrace();
     }

15b1db6ebd08bd986f9c2dd00226006c04f66510
Filip Jeremić <filipjeremic.it@gmail.com>
2025-02-01 21:32:04 +0100

    feat: added a simple file creation example

--- src/main/java/kettu/IO.java
+++ src/main/java/kettu/IO.java
@@ -3,6 +3,9 @@ package kettu;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class IO {
@@ -23,6 +26,18 @@ public class IO {
     try (BufferedReader reader = new BufferedReader(new FileReader("doesn't exist"))) {
       Stream<String> lines = reader.lines();
       lines.forEach(System.out::println);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public static void fileCreationExample() {
+
+    Path path = Paths.get("test.txt");
+
+    try {
+      Files.createFile(path);
     } catch (IOException e) {
       e.printStackTrace();
     }

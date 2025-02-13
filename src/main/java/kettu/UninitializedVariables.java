74ad952f8c8dd388d6f09da2eee897b811734984
Filip Jeremić <filipjeremic.it@gmail.com>
2025-02-13 08:57:14 +0100

    feat: added uninitialized variables examples

---
+++ src/main/java/kettu/UninitializedVariables.java
@@ -0,0 +1,70 @@
+package kettu;
+
+public class UninitializedVariables {
+
+  static boolean static_boolean;
+  static int static_int;
+  static double static_double;
+
+  static Boolean static_Boolean;
+  static Integer static_Integer;
+  static Double static_Double;
+  static String static_String;
+
+  boolean instance_boolean;
+  int instance_int;
+  double instance_double;
+
+  Boolean instance_Boolean;
+  Integer instance_Integer;
+  Double instance_Double;
+  String instance_String;
+
+  @SuppressWarnings("unused")
+  public static void examples() {
+
+    System.out.println("static_boolean: " + static_boolean);
+    System.out.println("static_int: " + static_int);
+    System.out.println("static_double: " + static_double);
+    System.out.println();
+
+    System.out.println("static_Boolean: " + static_Boolean);
+    System.out.println("static_Integer: " + static_Integer);
+    System.out.println("static_Double: " + static_Double);
+    System.out.println("static_String: " + static_String);
+    System.out.println();
+
+    UninitializedVariables uv = new UninitializedVariables();
+
+    System.out.println("instance_boolean: " + uv.instance_boolean);
+    System.out.println("instance_int: " + uv.instance_int);
+    System.out.println("instance_double: " + uv.instance_double);
+    System.out.println();
+
+    System.out.println("instance_Boolean: " + uv.instance_Boolean);
+    System.out.println("instance_Integer: " + uv.instance_Integer);
+    System.out.println("instance_Double: " + uv.instance_Double);
+    System.out.println("instance_String: " + uv.instance_String);
+    System.out.println();
+
+    boolean local_boolean;
+    int local_int;
+    double local_double;
+
+    Boolean local_Boolean;
+    Integer local_Integer;
+    Double local_Double;
+    String local_String;
+
+    // System.out.println("local_boolean: " + local_boolean);
+    // System.out.println("local_int: " + local_int);
+    // System.out.println("local_double: " + local_double);
+
+    // System.out.println("local_Boolean: " + local_Boolean);
+    // System.out.println("local_Integer: " + local_Integer);
+    // System.out.println("local_Double: " + local_Double);
+    // System.out.println("local_String: " + local_String);
+
+  }
+
+}

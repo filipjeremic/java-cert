cf19b2a3a2f5e709f67cd3088481569904a16aa1
Filip Jeremić <filipjeremic.it@gmail.com>
2025-02-13 09:14:30 +0100

    feat: added decimal precision examples

---
+++ src/main/java/kettu/DecimalPrecision.java
@@ -0,0 +1,25 @@
+package kettu;
+
+import java.math.BigDecimal;
+
+public class DecimalPrecision {
+
+  public static void precisionErrorExample() {
+
+    final double x = 0.012;
+    final double y = 3 * x;
+
+    System.out.println(y);
+
+  }
+
+  public static void bigDecimalExample() {
+
+    final BigDecimal x = new BigDecimal("0.12");
+    final BigDecimal y = x.multiply(new BigDecimal(3));
+
+    System.out.println(y);
+
+  }
+
+}

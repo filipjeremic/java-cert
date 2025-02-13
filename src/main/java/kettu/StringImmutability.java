0348aab2572c4ec5b356e8ece172bb69a402d55d
Filip Jeremić <filipjeremic.it@gmail.com>
2025-02-13 09:28:04 +0100

    feat: added String immutability examples

---
+++ src/main/java/kettu/StringImmutability.java
@@ -0,0 +1,31 @@
+package kettu;
+
+public class StringImmutability {
+
+  public static void example1() {
+
+    String s0 = "Hello";
+    String s1 = s0.replace('l', 'L');
+
+    System.out.println(s0);
+    System.out.println(s1);
+
+  }
+
+  public static void example2() {
+
+    char[] charArray = { 'H', 'e', 'l', 'l', 'o' };
+    String s = new String(charArray);
+
+    System.out.println(charArray);
+    System.out.println(s);
+
+    charArray[2] = 'L';
+    charArray[3] = 'L';
+
+    System.out.println(charArray);
+    System.out.println(s);
+
+  }
+
+}

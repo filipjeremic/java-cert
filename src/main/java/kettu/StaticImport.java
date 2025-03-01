08e273ab1d2bac101f836a94e61e4fc5edb0cba0
Filip Jeremić <filipjeremic.it@gmail.com>
2025-03-01 08:10:35 +0100

    feat: added a static import example

---
+++ src/main/java/kettu/StaticImport.java
@@ -0,0 +1,12 @@
+package kettu;
+
+import static java.lang.Math.*;
+import static java.lang.Integer.MIN_VALUE;
+
+public class StaticImport {
+  public static void example() {
+    System.out.println(sqrt(9));
+    System.out.println(pow(5, 3));
+    System.out.println(MIN_VALUE);
+  }
+}

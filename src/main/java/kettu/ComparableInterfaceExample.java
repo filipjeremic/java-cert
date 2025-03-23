2721ca50ab4d926e7874bd570a0e43c04ae222d5
Filip Jeremić <filipjeremic.it@gmail.com>
2025-03-23 22:23:52 +0100

    feat: added a ComparableInterface example

---
+++ src/main/java/kettu/ComparableInterfaceExample.java
@@ -0,0 +1,34 @@
+package kettu;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ComparableInterfaceExample {
+
+  private static class TestClass implements Comparable<TestClass> {
+
+    private final int value;
+
+    TestClass(final int value) {
+      this.value = value;
+    }
+
+    @Override
+    public int compareTo(TestClass tc) {
+      return value - tc.value;
+    }
+
+    @Override
+    public String toString() {
+      return String.valueOf(value);
+    }
+  }
+
+  public static void run() {
+    List<TestClass> list = Arrays.asList(new TestClass(12), new TestClass(7), new TestClass(25), new TestClass(2),
+        new TestClass(16));
+    System.out.println(list);
+    java.util.Collections.sort(list);
+    System.out.println(list);
+  }
+}

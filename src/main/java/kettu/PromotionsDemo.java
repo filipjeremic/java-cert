2c46db45d73a30fc49a858d6f1d5b385d4d87836
Filip Jeremić <filipjeremic.it@gmail.com>
2025-05-08 21:54:19 +0200

    feat: added the ternary operator promotions examples

--- src/main/java/kettu/PromotionsDemo.java
+++ src/main/java/kettu/PromotionsDemo.java
@@ -55,4 +55,144 @@ public class PromotionsDemo {
     // byte b2 = i; i is not a constant expression
   }
 
+  public static void ternaryOperatorExample() {
+    final byte fb = 1;
+    byte b = 1;
+    final short fs = 2;
+    short s = 2;
+    final int fi = 3;
+    int i = 3;
+    final int fiBig = 1_000_000;
+    int iBig = 1_000_000;
+    final long fl = 4;
+    long l = 4;
+    final float ff = 5.0f;
+    float f = 5.0f;
+    final double fd = 6.0;
+    double d = 6.0;
+
+    final boolean boolTrue = true;
+    final boolean boolFalse = false;
+
+    System.out.println(((Object) (boolTrue ? fb : fs)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fb : fs)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? b : s)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? b : s)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fb : fi)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fb : fi)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? b : i)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? b : i)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fb : fiBig)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fb : fiBig)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? b : iBig)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? b : iBig)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fs : fi)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fs : fi)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? s : i)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? s : i)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fs : fiBig)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fs : fiBig)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? s : iBig)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? s : iBig)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fb : fl)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fb : fl)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? b : l)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? b : l)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fs : fl)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fs : fl)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? s : l)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? s : l)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fi : fl)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fi : fl)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? i : l)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? i : l)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fiBig : fl)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fiBig : fl)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? iBig : l)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? iBig : l)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fb : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fb : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? b : f)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? b : f)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fs : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fs : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? s : f)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? s : f)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fi : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fi : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? i : f)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? i : f)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fiBig : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fiBig : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? iBig : f)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? iBig : f)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fl : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fl : ff)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? l : f)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? l : f)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fb : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fb : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? b : d)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? b : d)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fs : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fs : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? s : d)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? s : d)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fi : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fi : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? i : d)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? i : d)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fiBig : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fiBig : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? iBig : d)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? iBig : d)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? fl : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? fl : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? l : d)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? l : d)).getClass().getSimpleName());
+    System.out.println();
+
+    System.out.println(((Object) (boolTrue ? ff : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? ff : fd)).getClass().getSimpleName());
+    System.out.println(((Object) (boolTrue ? f : d)).getClass().getSimpleName());
+    System.out.println(((Object) (boolFalse ? f : d)).getClass().getSimpleName());
+    System.out.println();
+  }
+
 }

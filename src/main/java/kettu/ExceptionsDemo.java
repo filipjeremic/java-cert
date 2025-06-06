c605fea8b19a1778350b97da0adfb5ddc3e4b6d9
Filip Jeremić <filipjeremic.it@gmail.com>
2025-06-06 08:15:52 +0200

    feat: added the ExceptionsDemo class

---
+++ src/main/java/kettu/ExceptionsDemo.java
@@ -0,0 +1,11 @@
+package kettu;
+
+public class ExceptionsDemo {
+
+  {
+    // initializers can't throw exceptions
+    // throw new java.sql.SQLException();
+    // throw new RuntimeException();
+  }
+
+}

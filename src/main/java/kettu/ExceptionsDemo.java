f7d6d36858dabe3b3a6a26409f27334a680e9ff0
Filip Jeremić <filipjeremic.it@gmail.com>
2025-06-06 08:21:41 +0200

    feat: added a might throw example
    This method declares that it might throw an exception,
    even though it doesn't. A future implementation or
    subclass could throw the exception.

--- src/main/java/kettu/ExceptionsDemo.java
+++ src/main/java/kettu/ExceptionsDemo.java
@@ -1,11 +1,23 @@
 package kettu;
 
+import java.io.IOException;
+
 public class ExceptionsDemo {
 
   {
     // initializers can't throw exceptions
     // throw new java.sql.SQLException();
     // throw new RuntimeException();
+  }
+
+  public static void runExamples() {
+    try {
+      mightThrowInTheFutureExample();
+    } catch (IOException e) {
+    }
+  }
+
+  private static void mightThrowInTheFutureExample() throws IOException {
   }
 
 }
